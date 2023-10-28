28f90d40d61550a7a3883ae370d4feeb10b9929e
Ahmad Raza <ahmadraza7384@gmail.com>
2023-10-28 20:42:13 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 // add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
+const header = document.querySelector(".navbar");
 
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
+window.onscroll = function () {
+  var top = window.scrollY;
+  if (top >= 100) {
+    header.classList.add("navbarDark");
+  } else {
+    header.classList.remove("navbarDark");
+  }
 };
 
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
 
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-});
+function myFunction() {
+  var x = document.getElementById("#btn1");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+const btn = document.getElementById("#btn1");
+
